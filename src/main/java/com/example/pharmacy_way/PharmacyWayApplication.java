d7a8f4bcfe3de03ebf50805b90e79f4167b8e978
kyungstar <azure0563@gmail.com>
2025-01-28 19:37:15 +0900

    .

---
+++ src/main/java/com/example/pharmacy_way/PharmacyWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyWayApplication.class, args);
+    }
+
+}
