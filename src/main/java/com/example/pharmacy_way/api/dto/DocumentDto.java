2cfdd319efc0ac8800b0206852939fa2ddbe15b7
kyungstar <azure0563@gmail.com>
2025-03-16 22:29:03 +0900

    2025-03-16 : Kakao API Working.

---
+++ src/main/java/com/example/pharmacy_way/api/dto/DocumentDto.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_way.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentDto {
+
+
+}
---
+++ src/main/java/com/example/pharmacy_way/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.pharmacy_way.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class KakaoApiResponseDto {
+
+    @JsonProperty("address_name")
+    private String addressName;
+
+    @JsonProperty("y")
+    private double latitude;
+
+    @JsonProperty("x")
+    private double longitude;
+
+
+}
